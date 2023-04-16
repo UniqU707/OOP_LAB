cf8096bceef964aa336d5feb098ca6f846889548
UniqU707 <mnasonovy@gmail.com>
2023-04-16 16:12:33 +0400

    Optimization lab1

--- tests/figure_list_tests.cc
+++ tests/figure_list_tests.cc
@@ -3,37 +3,27 @@
 #define CAPACITY 10
 using namespace figure;
 
-
-TEST(FiguresListTests, FigureListConstructorTest)
-{
+FigureList construct() {
     const int SIZE = 4;
     Figure fig[CAPACITY];
 
-    Point p1 = Point(0, 0, 0);
-    Point p2 = Point(3, 3, 3);
-    fig[0] = Figure(BALL, p1, p2);
+    fig[0] = Figure(BALL, Point(0, 0, 0), Point(3, 3, 3));
 
-    Point p3;
-    Point p4;
-    p3 = Point(-5, 7, 5);
-    p4 = Point(-2, 7, -3);
-    fig[1] = Figure(CYLINDR, p3, p4);
+    fig[1] = Figure(CYLINDR, Point(-5, 7, 5), Point(-2, 7, -3));
 
-    Point p5;
-    Point p6;
-    p5 = Point(10, -15, 3);
-    p6 = Point(-4, 9, 10);
-    fig[2] = Figure(PARALLELEPIPED, p5, p6);
+    fig[2] = Figure(PARALLELEPIPED, Point(10, -15, 3), Point(-4, 9, 10));
 
-
-    Point p7;
-    Point p8;
-    p7 = Point(-5, -7, -9);
-    p8 = Point(1, 6, -4);
-    fig[3] = Figure(BALL, p7, p8);
+    fig[3] = Figure(BALL, Point(-5, -7, -9), Point(1, 6, -4));
 
     FigureList list = FigureList(fig, SIZE);
-    EXPECT_EQ(list.get_size(), SIZE);
+    
+    return list;
+}
+
+TEST(FiguresListTests, FigureListConstructorTest)
+{
+    FigureList list = construct();
+    EXPECT_EQ(list.get_size(), 4);
     EXPECT_EQ(list[0].get_figure_type(), BALL);
     EXPECT_EQ(list[1].get_figure_type(), CYLINDR);
     EXPECT_EQ(list[2].get_figure_type(), PARALLELEPIPED);
@@ -42,47 +32,14 @@ TEST(FiguresListTests, FigureListConstructorTest)
 }
 TEST(FiguresListTests, FigureListGetItemTest)
 {
-    const int SIZE = 4;
-    Figure fig[CAPACITY];
-
-    Point p1;
-    Point p2;
-    p1 = Point(0, 0, 0);
-    p2 = Point(3, 3, 3);
-    fig[0] = Figure(BALL, p1, p2);
-
-    Point p3;
-    Point p4;
-    p3 = Point(-5, 7, 5);
-    p4 = Point(-2, 7, -3);
-    fig[1] = Figure(CYLINDR, p3, p4);
-
-    Point p5;
-    Point p6;;
-    p5 = Point(10, -15, 3);
-    p6 = Point(-4, 9, 10);
-    fig[2] = Figure(PARALLELEPIPED, p5, p6);
-
-
-    Point p7;
-    Point p8;
-    p7 = Point(-5, -7, -9);
-    p8 = Point(1, 6, -4);
-    fig[3] = Figure(BALL, p7, p8);
-
-    FigureList list = FigureList(fig, SIZE);
+    FigureList list = construct();
     EXPECT_EQ(list[3].get_figure_type(), BALL);
 }
 
 TEST(FiguresListTests, FigureListAddItemTest)
 {
     FigureList list = FigureList();
-    Point p1;
-    Point p2;
-    p1 = Point(3, 3, 3);
-    p2 = Point(5, 7, 1);
-
-    Figure fig = Figure(CYLINDR, p1, p2);
+    Figure fig = Figure(CYLINDR, Point(3, 3, 3), Point(5, 7, 1));
 
     list.add_item(0, fig);
     EXPECT_EQ(list[0].get_figure_type(), CYLINDR);
@@ -92,41 +49,21 @@ TEST(FiguresListTests, FigureListAddItemTest)
     EXPECT_EQ(list[0].get_p2().get_x(), 5);
     EXPECT_EQ(list[0].get_p2().get_y(), 7);
     EXPECT_EQ(list[0].get_p2().get_z(), 1);
+    EXPECT_THROW(list[-1].figure_volume(), std::runtime_error);
+    EXPECT_THROW(list[10].figure_volume(), std::runtime_error);
+
+    FigureList list1 = FigureList();
+    Figure fig1 = Figure();
+    EXPECT_THROW(list1.add_item(1, fig1), std::runtime_error);
 }
 TEST(FiguresListTests, FigureListDelItemTest)
 {
-    const int SIZE = 4;
-    Figure fig[CAPACITY];
-
-    Point p1;
-    Point p2;
-    p1 = Point(0, 0, 0);
-    p2 = Point(3, 3, 3);
-    fig[0] = Figure(BALL, p1, p2);
-
-    Point p3;
-    Point p4;
-    p3 = Point(-5, 7, 5);
-    p4 = Point(-2, 7, -3);
-    fig[1] = Figure(CYLINDR, p3, p4);
-
-    Point p5;
-    Point p6;;
-    p5 = Point(10, -15, 3);
-    p6 = Point(-4, 9, 10);
-    fig[2] = Figure(PARALLELEPIPED, p5, p6);
-
-
-    Point p7;
-    Point p8;
-    p7 = Point(-5, -7, -9);
-    p8 = Point(1, 6, -4);
-    fig[3] = Figure(BALL, p7, p8);
-
-    FigureList list = FigureList(fig, SIZE);
+    FigureList list = construct();
     list.del_item(2);
     EXPECT_EQ(list.get_size(), 3);
     EXPECT_EQ(list[1].get_figure_type(), CYLINDR);
+    list.clear();
+    EXPECT_THROW(list.del_item(1), std::runtime_error);
 }
 TEST(FiguresListTests, FigureListClearTest)
 {
@@ -136,58 +73,9 @@ TEST(FiguresListTests, FigureListClearTest)
 }
 TEST(FiguresListTests, FigureListMaxVolumeTest)
 {
-    const int SIZE = 4;
-    Figure fig[CAPACITY];
-
-    Point p1;
-    Point p2;
-    p1 = Point(0, 0, 0);
-    p2 = Point(3, 3, 3);
-    fig[0] = Figure(BALL, p1, p2);
-
-    Point p3;
-    Point p4;
-    p3 = Point(-5, 7, 5);
-    p4 = Point(-2, 7, -3);
-    fig[1] = Figure(CYLINDR, p3, p4);
-
-    Point p5;
-    Point p6;;
-    p5 = Point(10, -15, 3);
-    p6 = Point(-4, 9, 10);
-    fig[2] = Figure(PARALLELEPIPED, p5, p6);
-
-    Point p7;
-    Point p8;
-    p7 = Point(-23, 1, 15);
-    p8 = Point(4, 1, 4);
-    fig[3] = Figure(CYLINDR, p7, p8);
-
-    FigureList list = FigureList(fig, SIZE);
+    FigureList list = construct();
     EXPECT_EQ(list.figure_max_volume(), 3);
-}
-TEST(FiguresListTests, FigureListMaxVolumeTestEmpty)
-{
-    FigureList list = FigureList();
-    EXPECT_EQ(list.figure_max_volume(), -1);
-}
-
-
-TEST(ExceptionTests, ExeptionIndexOutTest)
-{
-    FigureList list = FigureList();
-    EXPECT_THROW(list[-1].figure_volume(), std::runtime_error);
-    EXPECT_THROW(list[10].figure_volume(), std::runtime_error);
-}
-TEST(ExceptionTests, ExeptionCapacityTest)
-{
-    FigureList list = FigureList();
-    Figure fig = Figure();
-    EXPECT_THROW(list.add_item(1, fig), std::runtime_error);
-}
-TEST(ExceptionTests, ExeptionEmptyTest)
-{
-    FigureList list = FigureList();
     list.clear();
-    EXPECT_THROW(list.del_item(1), std::runtime_error);
+    EXPECT_EQ(list.figure_max_volume(), -1);
+
 }
--- tests/figure_tests.cc
+++ tests/figure_tests.cc
@@ -39,19 +39,11 @@ TEST(FiguresTests, FigureDefaultConstructorPosTest) {
     EXPECT_EQ(fig.get_p2().get_z(), 1);
 }
 TEST(FiguresTests, FigureConstructorTypeTest) {
-    Point p1;
-    Point p2;
-    p1 = Point(-2, 2, 9);
-    p2 = Point(1, 2, 15);
-    Figure fig = Figure(BALL, p1, p2);
+    Figure fig = Figure(BALL, Point(-2, 2, 9), Point(1, 2, 15));
     EXPECT_EQ(fig.get_figure_type(), BALL);
 }
 TEST(FiguresTests, FigureConstructorPosTest) {
-    Point p1;
-    Point p2;
-    p1 = Point(-2, 2, 9);
-    p2 = Point(1, 2, 15);
-    Figure fig = Figure(BALL, p1,p2);
+    Figure fig = Figure(BALL, Point(-2, 2, 9), Point(1, 2, 15));
     EXPECT_EQ(fig.get_p1().get_x(), -2);
     EXPECT_EQ(fig.get_p1().get_y(), 2);
     EXPECT_EQ(fig.get_p1().get_z(), 9);
@@ -61,280 +53,100 @@ TEST(FiguresTests, FigureConstructorPosTest) {
 }
 
 TEST(FiguresTests, FigureBallVolumeTest) {
-    Point p1;
-    Point p2;
-    p1 = Point(0, 0, 0);
-    p2 = Point(3, 3, 3);
-    Figure fig = Figure(BALL, p1,p2);
+    Figure fig = Figure(BALL, Point(0, 0, 0), Point(3, 3, 3));
     EXPECT_LT(fig.figure_volume(), 588);
     EXPECT_GT(fig.figure_volume(), 587);
 
-    Point p3;
-    Point p4;
-    p3 = Point(5, 4, 3);
-    p4 = Point(8, 1, 3);
-    Figure fig1 = Figure(BALL,p3,p4);
+    Figure fig1 = Figure(BALL, Point(5, 4, 3), Point(8, 1, 3));
     EXPECT_LT(fig1.figure_volume(), 320);
     EXPECT_GT(fig1.figure_volume(), 319);
 
-    Point p5;
-    Point p6;
-    p5 = Point(-1, 8, 3);
-    p6 = Point(-4, -2, 9);
-    Figure fig2 = Figure(BALL,p5,p6);
+    Figure fig2 = Figure(BALL, Point(-1, 8, 3), Point(-4, -2, 9));
     EXPECT_LT(fig2.figure_volume(), 7313);
     EXPECT_GT(fig2.figure_volume(), 7312);
 
-    Point p7;
-    Point p8;
-    p7 = Point(-5, -7, -9);
-    p8 = Point(1, 6, -4);
-    Figure fig3 = Figure(BALL, p7,p8);
+    Figure fig3 = Figure(BALL, Point(-5, -7, -9), Point(1, 6, -4));
     EXPECT_LT(fig3.figure_volume(), 14609);
     EXPECT_GT(fig3.figure_volume(), 14608);
 }
 TEST(FiguresTests, FigureBALLSurfaceAreaTest) {
-    Point p1;
-    Point p2;
-    p1 = Point(0, 0, 0);
-    p2 = Point(3, 3, 3);
-    Figure fig = Figure(BALL, p1,p2);
+    Figure fig = Figure(BALL, Point(0, 0, 0), Point(3, 3, 3));
     EXPECT_LT(fig.figure_surface_area(), 340);
     EXPECT_GT(fig.figure_surface_area(), 339);
 
-    Point p3;
-    Point p4;
-    p3 = Point(5, 4, 3);
-    p4 = Point(8, 1, 3);
-    Figure fig1 = Figure(BALL, p3,p4);
+    Figure fig1 = Figure(BALL, Point(5, 4, 3), Point(8, 1, 3));
     EXPECT_LT(fig1.figure_surface_area(), 227);
     EXPECT_GT(fig1.figure_surface_area(), 226);
 
-    Point p5;
-    Point p6;
-    p5 = Point(-1, 8, 3);
-    p6 = Point(-4, -2, 9);
-    Figure fig2 = Figure(BALL, p5,p6);
+    Figure fig2 = Figure(BALL, Point(-1, 8, 3), Point(-4, -2, 9));
     EXPECT_LT(fig2.figure_surface_area(), 1822);
     EXPECT_GT(fig2.figure_surface_area(), 1821);
 
-    Point p7;
-    Point p8;
-    p7 = Point(-5, -7, -9);
-    p8 = Point(1, 6, -4);
-    Figure fig3 = Figure(BALL, p7,p8);
+    Figure fig3 = Figure(BALL, Point(-5, -7, -9), Point(1, 6, -4));
     EXPECT_LT(fig3.figure_surface_area(), 2890);
     EXPECT_GT(fig3.figure_surface_area(), 2889);
 }
 
 TEST(FiguresTests, FigureCYLINDRVolumeTest) {
-    Point p1;
-    Point p2;
-    p1 = Point(0, 3, 5);
-    p2 = Point(0, 6, 7);
-    Figure fig = Figure(CYLINDR, p1,p2);
+    Figure fig = Figure(CYLINDR, Point(0, 3, 5), Point(0, 6, 7));
     EXPECT_LT(fig.figure_volume(), 57);
     EXPECT_GT(fig.figure_volume(), 56);
 
-    Point p3;
-    Point p4;
-    p3 = Point(-5, 7, 5);
-    p4 = Point(-2, 7, -3);
-    Figure fig1 = Figure(CYLINDR, p3,p4);
+    Figure fig1 = Figure(CYLINDR, Point(-5, 7, 5), Point(-2, 7, -3));
     EXPECT_LT(fig1.figure_volume(), 227);
     EXPECT_GT(fig1.figure_volume(), 226);
 
-    Point p5;
-    Point p6;
-    p5 = Point(15, 3, 8);
-    p6 = Point(15, -2, -7);
-    Figure fig2 = Figure(CYLINDR, p5,p6);
+    Figure fig2 = Figure(CYLINDR, Point(15, 3, 8), Point(15, -2, -7));
     EXPECT_LT(fig2.figure_volume(), 1178);
     EXPECT_GT(fig2.figure_volume(), 1177);
 
-    Point p7;
-    Point p8;
-    p7 = Point(-23, 1, 15);
-    p8 = Point(4, 1, 4);
-    Figure fig3 = Figure(CYLINDR, p7,p8);
+    Figure fig3 = Figure(CYLINDR, Point(-23, 1, 15), Point(4, 1, 4));
     EXPECT_LT(fig3.figure_volume(), 25189);
     EXPECT_GT(fig3.figure_volume(), 25188);
 }
 TEST(FiguresTests, FigureCYLINDRVSurfaceAreaTest) {
-    Point p1;
-    Point p2;
-    p1 = Point(0, 3, 5);
-    p2 = Point(0, 6, 7);
-    Figure fig = Figure(CYLINDR, p1, p2);
+    Figure fig = Figure(CYLINDR, Point(0, 3, 5), Point(0, 6, 7));
     EXPECT_LT(fig.figure_surface_area(), 95);
     EXPECT_GT(fig.figure_surface_area(), 94);
 
-    Point p3;
-    Point p4;
-    p3 = Point(-5, 7, 5);
-    p4 = Point(-2, 7, -3);
-    Figure fig1 = Figure(CYLINDR, p3, p4);
+    Figure fig1 = Figure(CYLINDR, Point(-5, 7, 5), Point(-2, 7, -3));
     EXPECT_LT(fig1.figure_surface_area(), 208);
     EXPECT_GT(fig1.figure_surface_area(), 207);
 
-    Point p5;
-    Point p6;
-    p5 = Point(15, 3, 8);
-    p6 = Point(15, -2, -7);
-    Figure fig2 = Figure(CYLINDR, p5, p6);
+    Figure fig2 = Figure(CYLINDR, Point(15, 3, 8), Point(15, -2, -7));
     EXPECT_LT(fig2.figure_surface_area(), 629);
     EXPECT_GT(fig2.figure_surface_area(), 628);
 
-    Point p7;
-    Point p8;
-    p7 = Point(-23, 1, 15);
-    p8 = Point(4, 1, 4);
-    Figure fig3 = Figure(CYLINDR, p7, p8);
+    Figure fig3 = Figure(CYLINDR, Point(-23, 1, 15), Point(4, 1, 4));
     EXPECT_LT(fig3.figure_surface_area(), 6446);
     EXPECT_GT(fig3.figure_surface_area(), 6445);
 }
 
 TEST(FiguresTests, FigurePARALLELEPIPEDVolumeTest) {
-    Point p1;
-    Point p2;
-    p1 = Point(-5, -7, -9);
-    p2 = Point(1, 6, -4);
-    Figure fig = Figure(PARALLELEPIPED, p1,p2);
+    Figure fig = Figure(PARALLELEPIPED, Point(-5, -7, -9), Point(1, 6, -4));
     EXPECT_EQ(fig.figure_volume(), 390);
 
-    Point p3;
-    Point p4;
-    p3 = Point(0, 4, -3);
-    p4 = Point(7, 12, -5);
-    Figure fig1 = Figure(PARALLELEPIPED, p3,p4);
+    Figure fig1 = Figure(PARALLELEPIPED, Point(0, 4, -3), Point(7, 12, -5));
     EXPECT_EQ(fig1.figure_volume(), 112);
 
-    Point p5;
-    Point p6;;
-    p5 = Point(10, -15, 3);
-    p6 = Point(-4, 9, 10);
-    Figure fig2 = Figure(PARALLELEPIPED, p5,p6);
+    Figure fig2 = Figure(PARALLELEPIPED, Point(10, -15, 3), Point(-4, 9, 10));
     EXPECT_EQ(fig2.figure_volume(), 2352);
 
-    Point p7;
-    Point p8;
-    p7 = Point(7, 4, 0);
-    p8 = Point(-24, -11, 9);
-    Figure fig3 = Figure(PARALLELEPIPED, p7,p8);
+    Figure fig3 = Figure(PARALLELEPIPED, Point(7, 4, 0), Point(-24, -11, 9));
     EXPECT_EQ(fig3.figure_volume(), 4185);
 
 }
 TEST(FiguresTests, FigurePARALLELEPIPEDSurfaceAreaTest) {
-    Point p1;
-    Point p2;
-    p1 = Point(-5, -7, -9);
-    p2 = Point(1, 6, -4);
-    Figure fig = Figure(PARALLELEPIPED, p1, p2);
+    Figure fig = Figure(PARALLELEPIPED, Point(-5, -7, -9), Point(1, 6, -4));
     EXPECT_EQ(fig.figure_surface_area(), 346);
 
-    Point p3;
-    Point p4;
-    p3 = Point(0, 4, -3);
-    p4 = Point(7, 12, -5);
-    Figure fig1 = Figure(PARALLELEPIPED, p3, p4);
+    Figure fig1 = Figure(PARALLELEPIPED, Point(0, 4, -3), Point(7, 12, -5));
     EXPECT_EQ(fig1.figure_surface_area(), 172);
 
-    Point p5;
-    Point p6;;
-    p5 = Point(10, -15, 3);
-    p6 = Point(-4, 9, 10);
-    Figure fig2 = Figure(PARALLELEPIPED, p5, p6);
+    Figure fig2 = Figure(PARALLELEPIPED, Point(10, -15, 3), Point(-4, 9, 10));
     EXPECT_EQ(fig2.figure_surface_area(), 1204);
 
-    Point p7;
-    Point p8;
-    p7 = Point(7, 4, 0);
-    p8 = Point(-24, -11, 9);
-    Figure fig3 = Figure(PARALLELEPIPED, p7, p8);
+    Figure fig3 = Figure(PARALLELEPIPED, Point(7, 4, 0), Point(-24, -11, 9));
     EXPECT_EQ(fig3.figure_surface_area(), 1758);
 
-}
-
-#include <figure/figure.h>
-#include <cmath>
-#include <stdexcept>
-
-using namespace figure;
-using namespace std;
-
-FigureList::FigureList() :_size(0) {
-
-}
-
-FigureList::FigureList(Figure figures[], int size) {
-	_size = size;
-	for (int i = 0; i < size; ++i) {
-		_figures[i] = figures[i];
-	}
-}
-
-Figure FigureList::get_figure_by_index(int i)const {
-	if (i < 0 || i>_size) {
-		throw std::runtime_error("Index out of range.");
-	}
-	return _figures[i];
-}
-
-int FigureList::get_size()const { return _size; }
-
-Figure FigureList::operator[](int ind) const {
-	if (ind < 0 || ind >= _size) {
-		throw std::runtime_error("Index out of range.");
-	}
-	return _figures[ind];
-}
-
-Figure& FigureList::operator[](int ind) {
-	if (ind < 0 || ind >= _size) {
-		throw std::runtime_error("Index out of range.");
-	}
-	return _figures[ind];
-}
-
-void FigureList::add_item(int ind, Figure fig) {
-	++_size;
-	if (_size == CAPACITY) {
-		throw std::runtime_error("Full capacity reached.");
-	}
-	if (ind < 0 || ind >= _size) {
-		throw std::runtime_error("Index out of range.");
-	}
-	for (int i = _size - 1; i >= ind; --i)
-	{
-		_figures[i] = _figures[i - 1];
-	}
-	_figures[ind] = fig;
-}
-
-void FigureList::clear() { _size = 0; }
-
-void FigureList::del_item(int ind) {
-	if (_size <= 0) {
-		throw std::runtime_error("FigureList is empty.");
-	}
-	for (int i = ind; i < _size - 1; ++i)
-	{
-		_figures[i] = _figures[i + 1];
-	}
-	--_size;
-}
-
-int FigureList::figure_max_volume()const {
-	if (_size == 0)
-		return -1;
-	int ind = 0;
-	double max_volume = _figures[0].figure_volume();
-	for (int i = 1; i < _size; ++i) {
-		double cur_volume = _figures[i].figure_volume();
-		if (cur_volume > max_volume) {
-			ind = i;
-			max_volume = cur_volume;
-		}
-	}
-	return ind;
 }
